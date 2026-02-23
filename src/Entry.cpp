26809caf30345ae150ad9fd2cf51c5d4cf4a959e
MisledWater79 <misledwater79@gmail.com>
2026-02-23 15:23:01 -0500

    Fix names

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -1,7 +1,7 @@
 #include "DynamicLights.hpp"
 
-ENDSTONE_PLUGIN("endstone_dynamic_lights", "1.0.0", DynamicLights) {
+ENDSTONE_PLUGIN("dynamic_lights", "1.0.0", DynamicLights) {
     authors = {"MisledWater79"};
     description = "Endstone plugin to add dynamic lights to the game.";
-    prefix = "mw";
+    prefix = "MW79";
 }
